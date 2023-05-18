e5bc5f3ae3dd007f91f4694991e749117d05cff9
vgav855 <132547267+vgav855@users.noreply.github.com>
2023-05-18 08:56:36 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,69 @@
-
+let d = 0
+basic.forever(function () {
+    d = input.compassHeading()
+    if (Math.abs(d - 45) <= 22) {
+        basic.showLeds(`
+            # . . . .
+            . # . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (Math.abs(d - 90) <= 22) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (Math.abs(d - 135) <= 22) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . # . . .
+            # . . . .
+            `)
+    } else if (Math.abs(d - 180) <= 22) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . # . .
+            . . # . .
+            `)
+    } else if (Math.abs(d - 225) <= 22) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . # .
+            . . . . #
+            `)
+    } else if (Math.abs(d - 270) <= 22) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # # #
+            . . . . .
+            . . . . .
+            `)
+    } else if (Math.abs(d - 315) <= 22) {
+        basic.showLeds(`
+            . . . . #
+            . . . # .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
